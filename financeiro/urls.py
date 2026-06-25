4dffc34eca33ba5d65fa75bb5aa4a14f542e3105
andreprog02 <andre_santos_02@yahoo.com.br>
2026-06-24 21:12:18 -0300

    teste

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
-app_name = 'financeiro'
+app_name = "financeiro"
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('estornar/<int:transacao_id>/', views.estornar, name='estornar'),
+
+    # Caixa
+    path('caixa/', views.caixa_painel, name='caixa_painel'),
+    path('caixa/abrir/', views.caixa_abrir, name='caixa_abrir'),
+    path('caixa/fechar/', views.caixa_fechar, name='caixa_fechar'),
+    path('caixa/<int:caixa_id>/', views.caixa_detalhe, name='caixa_detalhe'),
 ]
